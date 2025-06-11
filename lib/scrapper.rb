f54ff6c4f68421cd2db1b73248bcee2e0627d1a2
Bartosz <bjedrzejewski03+fork@gmail.com>
2025-06-11 13:52:18 +0200

    Fix bugs

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -4,8 +4,6 @@ require 'prawn'
 require 'sidekiq'
 require 'net/http'
 
-
-
 class DataScapper
   BASE_URL = 'https://www.auto-data.net'
 
@@ -114,68 +112,64 @@ class DataScapper
     end
     configs
   end
-
+    def count_matching_keys(headers, mapa)
+      headers.count { |h| mapa.key?(h) }
+    end
 
   def technical_data(config_url)
-    puts config_url
-    puts "Techniczne dane: #{config_url}"
-    doc = Nokogiri::HTML(URI.open(config_url))
 
-    file_name="Report_#{DateTime.now.strftime("%Y%m%d_%H%M%S").to_s}.csv"
-    file_name1="Report_#{DateTime.now.strftime("%Y%m%d_%H%M%S").to_s}x.csv"
+    file_name = "Report_#{DateTime.now.strftime('%Y%m%d_%H%M%S')}.csv"
+    head = ["Pełna nazwa","Marka","Model","Link","Liczba drzwi","Liczba miejsc","Średnica zawracania","Promień skrętu","Długość","Szerokość","Szerokość z lusterkami bocznymi","Wysokość","Rozstaw osi","Rozstaw kół - przód","Rozstaw kół - tył","Zwis przedni","Zwis tylny","Prześwit","Długość z hakiem holowniczym","Szerokość ze złożonymi lusterkami bocznymi","Szerokość przy otwartych drzwiach z przodu","Szerokość przy otwartych drzwiach z tyłu","Wysokość z relingami dachowymi","Wysokość z anteną","Wysokość przy otwartej klapie bagażnika","Wysokość przy otwartej pokrywie silnika","Prześwit 4x4","Odległość oparcia fotela przedniego od kierownicy","Długość kolumny kierownicy","Odległość oparcia przedniego od komory silnika","Odległość od siedziska przedniego do dachu","Wysokość siedziska przedniego","Długość siedziska przedniego","Wysokość oparcia przedniego","Odległość pomiędzy siedzeniami przednimi i tylnymi","Odległość od siedziska tylnego do dachu","Odległość od podłogi do siedziska tylnego","Długość siedziska tylnego","Wysokość oparcia tylnego","Szerokość nad podłokietnikami z przodu","Szerokość nad podłokietnikami z tyłu","Szerokość na wysokości podłokietników z przodu","Szerokość na wysokości podłokietników z tyłu","Przestrzeń wsiadania z przodu - szerokość","Przestrzeń wsiadania z przodu - wysokość","Przestrzeń wsiadania z tyłu - szerokość","Przestrzeń wsiadania z tyłu - wysokość","Zakres przesuwania foteli przednich","Całkowita długość wnętrza kabiny","Całkowita szerokość wnętrza kabiny","Całkowita wysokość wnętrza kabiny","Maksymalna pojemność bagażnika (siedzenia złożone)","Minimalna pojemność bagażnika (siedzenia rozłożone)","Szerokość pomiędzy nadkolami","Wysokość bagażnika","Szerokość bagażnika","Długość do oparcia tylnej kanapy","Długość ze złożoną tylną kanapą","Wysokość progu załadowczego","Kąt natarcia","Kąt rampowy","Kąt zejścia","Kąt przechyłu bocznego","Możliwość podjazdu","Głębokość brodzenia","Maksymalna ładowność","Dopuszczalne obciążenie dachu","Powierzchnia „przejrzysta” przedniej szyby","Całkowita powierzchnia „przejrzysta” szyb","Produkowany","Pojemność skokowa","Typ silnika","Moc silnika","Maksymalny moment obrotowy","Moc silnika (spalinowy)","Maksymalny moment obrotowy (spalinowy)","Moc silnika (elektryczny)","Maksymalny moment obrotowy (elektryczny)","Montaż silnika","Doładowanie","Umiejscowienie wałka rozrządu","Liczba cylindrów","Układ cylindrów","Liczba zaworów","Stopień sprężania","Zapłon","Typ wtrysku","Liczba silników","Średnica cylindra × skok tłoka","Układ paliwowy","Dodatkowe informacje","Rodzaj układu kierowniczego","Opony podstawowe","Opony opcjonalne","Felgi podstawowe","Felgi opcjonalne","Rozstaw śrub","Rodzaj hamulców (przód)","Rodzaj hamulców (tył)","Hamowanie (100 do 0km/h) z ABS","Typ układu hamulcowego","Grubość tarcz hamulcowych (przód)","Grubość tarcz hamulcowych (tył)","Średnica tarcz hamulcowych (przód)","Średnica tarcz hamulcowych (tył)","Rodzaj zawieszenia (przód)","Rodzaj zawieszenia  (tył)","Amortyzatory","Rodzaj skrzyni","Nazwa skrzyni","Liczba stopni","Rodzaj napędu","Rodzaj sprzęgła","Prędkość maksymalna","Przyspieszenie (od 0 do 100km/h)","400 metrów ze startu zatrzymanego","1000 metrów ze startu zatrzymanego","Średnie spalanie (cykl mieszany)","Spalanie na trasie (na autostradzie)","Spalanie w mieście","Pojemność akumulatora brutto","Typ ładowarki","Chłodzenie akumulatora","Pojemność akumulatora netto","Metodologia pomiaru zasięgu","Zużycie energii","Maksymalny zasięg przy oszczędnej jeździe na długiej trasie","Średni maksymalny zasięg","Średni minimalny zasięg","Maksymalna moc ładowania DC","Maksymalna moc ładowania AC","Stacja szybkiego ładowania","Gniazdko 3F/Stacja AC","Gniazdko 1F","Pojemność zbiornika paliwa","Zasięg (cykl mieszany)","Zasięg (autostrada)","Zasięg (miasto)","Emisja CO₂","Norma emisji spalin","Minimalna masa własna pojazdu (bez obciążenia)","Maksymalna masa całkowita pojazdu (w pełni obciążonego)","Maksymalna masa przyczepy z hamulcami","Maksymalna masa przyczepy bez hamulców","Maksymalny nacisk na hak","Pojemność akumulatora","Pojemność akumulatora w wersji z klimatyzacja","System start&stop","Kod silnika"]
 
+    CSV.open(file_name, 'w') do |csv|
+      config_url.each do |line|
+      doc = Nokogiri::HTML(URI.open(line))
 
-    CSV.open(file_name1, 'w') do |csv|
-      table = doc.xpath("/html/body/div[2]/table")
+        table = doc.xpath("/html/body/div[2]/table")
+        data_map = {}
+        final_string = ""
+        table.search('tr').each do |row|
+          next if row.css('.no, .no2').any?
 
-      table.search('tr').each do |row|
-        if row.css('.no').any?
-          next
-        end
-        if row.css('.no2').any?
-          next
-        end
-        th = row.search('th')
-        td = row.search('td')
-        if td.nil? || td.nil?
-          next
-        end
-        csv << [th.text,td.text]
-        #puts "#{th.text} : #{td.text}"
-      end
-     end
+          th = row.at('th')
+          td = row.at('td')
 
-    # CSV.open(file_name, 'w') do |csv|
-    #   csv << ['Key', 'Value']
-    #
-    #     doc.xpath('/html/body/div[2]/table').each do |table|
-    #         table.xpath('.//tr').each do |row|
-    #             if row.xpath('./th[not(contains(@class, "no")) or @colspan="2"]').any?
-    #             # key = row.xpath('./th').text.strip
-    #             # value = row.xpath('./td').text.strip
-    #             komorki = row.xpath('./td')
-    #             unless komorki.count == 2
-    #
-    #               key = komorki.first.text.strip
-    #               value = komorki.last.text.strip
-    #             end
-    #
-    #             csv << [key,value] unless key.nil? || value.nil?
-    #           end
-    #         end
-    #       end
+          next if th.nil? || td.nil?
 
-      doc.css('table.tech.specs').each do |table|
-        table.css('tr').each do |row|
-          key = row_at.css('th')&.text&.strip
-          value = row_at.css('td')&.text&.strip
+          key = th.text.gsub(/\s+/, ' ').strip
+          value = td.text.gsub(/\s+/, ' ').strip
+          value = value.gsub(";"," ")
 
-          if key && value && !key.empty? && !value.empty?
-            csv  << [key, value]
+          if key == "Długość" || key == "Szerokość" || key == "Wysokość" || key == "Rozstaw osi"
+            value = convert_measure(value)
           end
-        end
+          data_map[key] = value
+
+          end
+          head.each do |header|
+            value = data_map[header]
+            if header == "Link"
+              final_string += "#{line};"
+            elsif value.nil? || value == "" || value == "Zaloguj się, aby zobaczyć."
+              final_string << "Brak danych;"
+            else
+              final_string += "#{value.to_s};"
+            end
+          end
+      csv << final_string.split(";")
       end
     end
   end
 
+  def convert_measure(data)
+    return  data[/\d+\s*mm/]
+  end
+
+  def delete(data)
+    if data.include?("mm")
+      data.split("mm").first
+    else
+      data
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -5,26 +5,13 @@ require 'csv'
 scrapper = DataScapper.new
 
 all = scrapper.scrap_brands
-
 models = scrapper.scrap_model(all[22])
-
 version = scrapper.scrap_version(models[0])
-
 config = scrapper.scrap_config(version[1])
+scrapper.technical_data(config)
 
-puts
-puts "Oto url = #{config[0]}"
-puts
-
-scrapper.technical_data(config[1])
-#
-# models.each do |model|
-#   scrapper.scrap_version(model)
-# end
-
-
-models = []
-#
-# all.each do |brand|
-#   models < scrapper.scrap_model(brand)
-# end
+# all = scrapper.scrap_brands
+# models = scrapper.scrap_model(all)
+# version = scrapper.scrap_version(models)
+# config = scrapper.scrap_config(version)
+# scrapper.technical_data(config)
